13b4987bc0188984737ea1b45baaf1d019a40213
l00590538 <h2m0r0r0@gmail.com>
2022-04-18 15:59:38 +0900

    Readme for game logger on Unity platform

---
+++ unity/Assets/GameLogger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace com.sail.gamelogger
+{
+    public static class Gamelogger
+    {
+        public static void onEvent(string eventName)
+        {
+            using (var gamelogger = new AndroidJavaObject("com.sail.gamelogger.Gamelogger"))
+            {
+                gamelogger.CallStatic("onEvent", eventName);
+            } 
+        }
+    }
+}
